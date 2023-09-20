833fe138cac9bf8c0c9f93f981f78422fa899b86
Felix-Okeyo <felixokeyo20@gmail.com>
2023-09-20 21:38:50 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,17 @@
-import requests
-import json
+import requests  # Import the requests library for making HTTP requests
+import json  # Import the json library for working with JSON data
 
 class GetRequester:
-
+  
     def __init__(self, url):
-        self.url = url
-
+        self.url = url  # Initialize the class with a URL attribute
+    
     def get_response_body(self):
-        pass
-
+        # Method to send an HTTP GET request to the URL and return the response content
+        response = requests.get(self.url)
+        return response.content
+    
     def load_json(self):
-        pass
+        # Method to load JSON data from the response content obtained from the URL
+        response_body = self.get_response_body()
+        return json.loads(response_body)
